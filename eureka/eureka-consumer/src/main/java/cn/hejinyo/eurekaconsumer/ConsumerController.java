c8aa6d8e3b355625fb145aae2aa00b7e87945bb8
HejinYo <hejinyo@gmail.com>
2018-04-18 23:39:12 +0800

    消费者例子

---
+++ eureka/eureka-consumer/src/main/java/cn/hejinyo/eurekaconsumer/ConsumerController.java
@@ -0,0 +1,22 @@
+package cn.hejinyo.eurekaconsumer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author : HejinYo   hejinyo@gmail.com
+ * @date :  2018/4/18 23:29
+ */
+@RestController
+public class ConsumerController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @GetMapping("/ribbon-consumer")
+    public String helloConsumer() {
+        return restTemplate.getForEntity("http://HELLO-SERVICE/hello", String.class).getBody();
+    }
+}
