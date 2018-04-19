4be5b907dafadbd625c329389cc48f6b9f675b1e
HejinYo <hejinyo@gmail.com>
2018-04-19 23:48:04 +0800

    消费者例子

--- eureka/eureka-consumer/src/main/java/cn/hejinyo/eurekaconsumer/ConsumerController.java
+++ eureka/eureka-consumer/src/main/java/cn/hejinyo/eurekaconsumer/ConsumerController.java
@@ -7,7 +7,7 @@ import org.springframework.web.client.RestTemplate;
 
 /**
  * @author : HejinYo   hejinyo@gmail.com
- * @date :  2018/4/18 23:29
+ * @date :  2018/4/18 23:30
  */
 @RestController
 public class ConsumerController {
