c48c927bb5425d2d3c62908e9538113b6a4e720c
HejinYo <hejinyo@gmail.com>
2018-04-27 00:11:43 +0800

    高可用注冊中心

---
+++ eureka/eureka-service-back/src/main/java/cn/hejinyo/EurekaServerBackApplication.java
@@ -0,0 +1,14 @@
+package cn.hejinyo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerBackApplication.class, args);
+    }
+}
